68a73dcb304a17f3de0e88288d58eaefc2415b93
vijay <vijaykumar14198@gmail.com>
2022-07-11 11:05:32 +0530

    Add new method in BinarySearchTree find_min() , find_max(),calculate_sum() , pre_order_trasversal()

--- Binary Tree/BinarySearchTree.py
+++ Binary Tree/BinarySearchTree.py
@@ -4,23 +4,27 @@ class BinarySearchTree:
         self.left=None
         self.right=None
 
-    def add_child(self,data):
-        if self.data==data:
+
+
+    def add_child(self,child):
+        if self.data==child:
             return # node already exist
         
-        elif data<self.data:
+        elif child<self.data:
             ##left subtree
             if self.left:
-                self.left.add_child(data)
+                self.left.add_child(child)
             else:
-                self.left= BinarySearchTree(data)
+                self.left= BinarySearchTree(child)
 
         else:
             ##right subtree
             if self.right:
-                self.right.add_child(data)
+                self.right.add_child(child)
             else:
-                self.right= BinarySearchTree(data)
+                self.right= BinarySearchTree(child)
+
+
 
     def in_order_transversal(self):
         elements=[]
@@ -36,7 +40,23 @@ class BinarySearchTree:
             elements += self.right.in_order_transversal()
 
         return elements
-            
+
+    
+    def pre_order_traversal(self):
+        elements = []
+        elements.append(self.data)
+
+        #visit left subtree
+        if self.left:
+            elements += self.left.pre_order_traversal()
+
+        #visit right subtree
+        if self.right:
+            elements += self.right.pre_order_traversal()
+        
+        return elements
+
+
 
     def search(self,val):
         if self.data==val:
@@ -53,9 +73,27 @@ class BinarySearchTree:
                 return self.right.search(val)
             else:
                 return False
-
-
         
+
+    def find_min(self):
+        elements=self.in_order_transversal()
+        return elements[0]
+
+
+    def find_max(self):
+        elements=self.in_order_transversal()
+        return elements[len(elements)-1]
+
+    def calculate_sum(self):
+        elements = self.in_order_transversal()
+        element = 0
+        for i in range(0,len(elements)):
+            element+=elements[i]
+        return element
+
+    
+        
+
 
 
 
@@ -72,8 +110,12 @@ def build_tree(elements):
 
 
 if __name__=='__main__':
-    numbers= [ 87,45,67,24,56,78,89,90,2,1,4,5,6,7,8,90,67,5,4,56,7,87,89 ]
+    numbers= [ 87,45,67,24,56,78,89,90,876,-123,2,1,4,5,6,7,8,90,67,5,4,56,7,87,89,2 ]
     root=build_tree(numbers)
     print("in order trasversal",root.in_order_transversal())
     print(root.search(0))
+    print(root.find_min())
+    print(root.find_max())
+    print(root.calculate_sum())
+    print("pre order transversal",root.pre_order_traversal())
     pass
